c12c2cb65dfeebb8f94776c943525a8b2ce08138
Susumu <sumura80@gmail.com>
2019-07-19 11:25:07 +0900

    Add ec-site

--- color_change_branch/script.js
+++ color_change_branch/script.js
@@ -4,84 +4,6 @@ var Month = Date.getMonth();
 var Days = Date.getDate();
 var Hours = Date.getHours();
 var Mins = Date.getMinutes();
-
-Vue.component('my-product',{
-	template:`
-	<div class="wrapMain container mt-4">
-		<div class="row">
-			<div class="col-sm-6 product-image mt-2">
-				 <img class="makaron-img-size img-thumbnail" v-bind:src="image" alt="makaron-image">
-			</div><!-- end of product-image-->
-			
-			<div class="col-sm-6  product-info mt-2">
-
-				<h2>{{ product }}</h2>
-				<p v-if="inventory > 1">在庫あり</p>
-				<p v-else>在庫なし</p>
-				<ul>
-					<li v-for="item in details">{{ item }}</li>
-				</ul>
-
-				<div class="row makaronColorBox"><!-- makaron color box-->
-					<div v-for="(makaron,index) in makaronpoints" v-bind:key="makaron.makaronId" class="col-3 col-sm-3 mx-2 color-box" v-on:mouseover="updateMakaronImg(index)" v-bind:style='{backgroundColor: makaron.makaronColor}'>	 
-		 			</div>
-			</div><!-- makaron color box-->
-
-			<div class="button-box">
-				<div class="btn-color" v-on:click="AddToCart"  v-bind:class="{disableButton: !inventory}">カートに入れる</div>
-			</div><!-- end of button- box-->
-
-			
-
-			</div><!-- end of product-info -->
-		</div><!-- end of row -->
-	 
-	</div><!-- end of wrapMain -->
-	`,
-	data:function(){
-			return{
-	 product:'極上のマカロン',
-	 // image:'pink.jpg',
-
-	 //inventory:true,
-	 
-	 chosenmakaron:0, 
-	 details:[
-	 	'外は、サックリしていて中はしっとり','間にクリームが入っています','色とバリエーションが豊富','ダイエット中にもオススメ','注文ご2日以内にお届けします'
-	 	],
-	 	makaronpoints:[
-	 	{makaronId:1 ,makaronColor:'pink',makaronImage:'pink.jpg',makaronQuantity:10,makaronName:'ストロベリー'},
-	 	{makaronId:2 ,makaronColor:'lightgreen',makaronImage:'lightgreen.jpg' ,makaronQuantity:0 ,makaronName:'グリーンティー'},
-	 	{makaronId:3 ,makaronColor:'plum',makaronImage:'purple.jpg' ,makaronQuantity:5 ,makaronName:'カシス'},
-	 	{makaronId:4 ,makaronColor:'lightblue',makaronImage:'lightblue.jpg' ,makaronQuantity:7 ,makaronName:'ミント'},
-	 	{makaronId:5 ,makaronColor:'peru',makaronImage:'brown.jpg' ,makaronQuantity:4 ,makaronName:'チョコレート'},
-	 	{makaronId:6 ,makaronColor:'lemonchiffon',makaronImage:'yellow.jpg' ,makaronQuantity:0 ,makaronName:'レモン'}
-
-	 	]
-			} // end of return
-	  },  
-	  computed:{
-  	image:function(){
-  		return this.makaronpoints[this.chosenmakaron].makaronImage
-  	},
-  	inventory:function(){
-  		return this.makaronpoints[this.chosenmakaron].makaronQuantity
-  	}
-  },// end of computed
-  methods:{
-  	AddToCart:function(){
-  		this.$emit('increase-cart',this.makaronpoints[this.chosenmakaron].makaronName)
-  	},
-  	updateMakaronImg:function(index){
-  		this.chosenmakaron = index
-  		console.log(index)
-  	}// end of methods
-  }
-
-})
-
-
-
 new Vue({
 	el:"#app",
 	data:{
@@ -114,8 +36,7 @@ new Vue({
 	 carfoodfee:2870,
 	 carfoodtimes:'',
 	 traveldatasent:false,
-	 submitAgree:false,
-	 cart:[]
+	 submitAgree:false
 
 	},
 	methods:{
@@ -179,10 +100,7 @@ new Vue({
   		confirm:function(){
 	  		alert('送信してよろしいですか？')
 	  	　this.traveldatasent = !this.traveldatasent
-  		},// end of data 送信
-  		increaseCart:function(name){
-			this.cart.push(name)
-			}// end of increase cart	
+  		}// end of data 送信
 
 	}, //end of methods
 
